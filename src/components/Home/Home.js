963c78d3ada69cfae2c3b95b8196f6b1950676f7
Nyaine Chan Myint <45549387+NyaineChanMyint@users.noreply.github.com>
2023-02-21 15:14:27 +0630

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Hein Htet Win</strong>
+                <strong className="main-name"> Nyaine Chan Myint</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
